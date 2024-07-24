6b6f4873b482998a62eac83754d6c715dae82250
KubaJerz <Koobman.kj@gmail.com>
2024-07-24 16:03:07 -0400

    more changes no saving model yet

--- TrainLoops/tl00.py
+++ TrainLoops/tl00.py
@@ -14,7 +14,7 @@ IMPORTANT NOTES about this training script
 -we expect the forward pass to return logits
 -we pass logits into the criterion
 '''
-def plot_combined_metrics(lossi, devlossi, f1i, devf1i, epoch):
+def plot_combined_metrics(lossi, devlossi, f1i, devf1i, name):
     plt.figure(figsize=(12, 6))
     
     # Plot losses
@@ -36,7 +36,7 @@ def plot_combined_metrics(lossi, devlossi, f1i, devf1i, epoch):
     plt.legend()
     
     plt.tight_layout()
-    plt.savefig(f"training_metrics_epoch_{epoch}.png")
+    plt.savefig(f"{name}_train_metrics.png")
     plt.close()
 
 
@@ -47,7 +47,7 @@ def load_data(data_path, batch_size):
     dataset = YourDataset(data_path)
     return DataLoader(dataset, batch_size=batch_size, shuffle=True)
     
-def train(model, train_loader, val_loader, criterion, optimizer, device, epochs):
+def train(model, train_loader, test_loader, criterion, optimizer, device, epochs):
     lossi = [] #train_losses
     devlossi = [] #val_losses
     f1i = []  #train_f1_scores
@@ -56,10 +56,10 @@ def train(model, train_loader, val_loader, criterion, optimizer, device, epochs)
     model = model.to(device)
     
     model.train()
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs),desc=f'Progress: '):
         epoch_loss = 0
         epoch_f1 = 0
-        for X_batch, y_batch in tqdm(train_loader, desc=f'Epoch: {epoch+1}/{epochs}'):
+        for X_batch, y_batch in train_loader:
             X_batch, y_batch = X_batch.to(device), y_batch.to(device)
             
             optimizer.zero_grad()
@@ -80,16 +80,22 @@ def train(model, train_loader, val_loader, criterion, optimizer, device, epochs)
 
 
         with torch.no_grad():
-            devlogits = model.forward(X_test)
-            devloss = criterion(devlogits, y_test).item()
-            devf1 = multiclass_f1_score(devlogits, y_test, num_classes=model.num_classes).item()
-            devlossi.append(devloss)
-            devf1i.append(devf1)
+            test_epoch_loss = 0
+            test_epoch_f1 = 0
+            for X_batch, y_batch in test_loader:
+                X_batch, y_batch = X_batch.to(device), y_batch.to(device)
 
+                devlogits = model.forward(X_batch)
+                dev_loss = criterion(devlogits, y_batch).item()
+                dev_f1 = multiclass_f1_score(devlogits, y_batch, num_classes=model.num_classes).item() 
+                test_epoch_loss += dev_loss
+                test_epoch_f1 += dev_f1
 
-    
-    if (epoch+1) % 2 == 0:
-        plot_combined_metrics(lossi, devlossi, f1i, devf1i, epoch+1)
+            devlossi.append(test_epoch_loss / len(test_loader))
+            devf1i.append(test_epoch_f1 / len(test_loader))
+
+        if (epoch+1) % 2 == 0:
+            plot_combined_metrics(lossi, devlossi, f1i, devf1i, 'test')
 
 
 
