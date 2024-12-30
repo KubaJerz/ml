f94a4e2fcac0507035315992a90805b5511f33bc
KubaJerz <62904217+KubaJerz@users.noreply.github.com>
2024-12-30 13:46:05 +0100

    check feild now alos valuadtes list of values if is_seq=true) ' _

--- ml-pipline/src/utils.py
+++ ml-pipline/src/utils.py
@@ -1,14 +1,24 @@
 from typing import Dict, Any
 
-def check_field(config, field_name, field_type):
-    if field_name not in config:
-        raise ValueError(f"Missing {field_name}")
-    if not isinstance(config[field_name], (field_type)):
-        raise ValueError(f"{field_name} must be of type: {field_type.__name__}")
-    return True
-
-
 def check_section_exists(config, section_name):
     if section_name not in config:
         raise ValueError(f"Missing {section_name} section")
+    return True
+
+def check_field(config, field_name, field_type,is_sequence = False):
+    if field_name not in config:
+        raise ValueError(f"Missing {field_name}")
+        
+    value = config[field_name]
+    
+    if is_sequence:
+        if not isinstance(value, (list, tuple)):
+            raise ValueError(f"{field_name} must be a list or tuple")
+            
+        if not all(isinstance(item, field_type) for item in value):
+            raise ValueError(f"All items in {field_name} must be of type: {field_type.__name__}")
+    else:
+        if not isinstance(value, field_type):
+            raise ValueError(f"{field_name} must be of type: {field_type.__name__}")
+            
     return True
